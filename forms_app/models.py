88364b33211fd5a12c4dd1401d6fb85d1a163bc4
xXtloB <andreagaravaglia37@gmail.com>
2021-03-10 13:34:36 +0100

    finito

--- forms_app/models.py
+++ forms_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-#ciao
+
 # Create your models here.
+class Contatto(models.Model):
+    nome=models.CharField(max_length=100)
+    cognome=models.CharField(max_length=100)
+    email=models.EmailField(max_length=200)
+    contenuto=models.TextField()
--- forms_app/views.py
+++ forms_app/views.py
@@ -10,11 +10,13 @@ def contatti(request):
         form=FormContatto(request.POST)
 
         if form.is_valid():
-            print("Il form è valido")
-            print("Nome: ", form.cleaned_data["nome"])
-            print("Cognome: ",form.cleaned_data["cognome"])
-            print("Email: ",form.cleaned_data["email"])
-            print("Contenuto: ",form.cleaned_data["contenuto"])
+            print("Salvo il contatto nel database")
+            nuovo_contatto=form.save()
+            print("new post: ", nuovo_contatto)
+            print(nuovo_contatto.nome)
+            print(nuovo_contatto.cognome)
+            print(nuovo_contatto.email)
+            print(nuovo_contatto.contenuto)
 
             return HttpResponse("<h1>Grazie per averci contattato</h1>")
         else:
