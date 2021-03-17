ff95b8ab81fdcb487ff344f5a39da42183ae1467
xXtloB <71756309+xXtloB@users.noreply.github.com>
2021-03-17 12:34:06 +0100

    fine

--- forms_app/forms.py
+++ forms_app/forms.py
@@ -1,9 +1,26 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Contatto
 class FormContatto(forms.ModelForm):
     class Meta:
         model=Contatto
         fields="__all__"
+        widgets={
+            'nome':forms.TextInput(attrs={'placeholder':'compila questo campo','class':'form-control'}),
+            'cognome':forms.TextInput(attrs={'placeholder':'compila questo campo','class':'form-control'}),
+            'email':forms.TextInput(attrs={'placeholder':'compila questo campo','class':'form-control'}),
+            'Contenuto':forms.Textarea(attrs={'placeholder':'area testuale scrivi pure','class':'form-control'})
+        }
 
 
+    def clean_contenuto(self):
+        dati=self.cleaned_data["contenuto"]
+        if "parola" in dati:
+            raise ValidationError("Il contenuto inserito viola le norme del sito!")
+        
+        if len(dati)<20:
+            raise ValidationError("Il contenuto inserito è troppo breve")
+        return dati
+    
 
+    
